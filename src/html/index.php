0bee1f0de367c6f33b9faa0c5473dba96cce10f0
坂上　正 <t_sakagami@miraino-katachih2.co.jp>
2020-02-01 08:11:46 +0900

    今月のついたちの計算方法を変更した。

--- src/html/index.php
+++ src/html/index.php
@@ -2,35 +2,48 @@
 // 今月を0とする。
 $month = 0;
 
-// GETパラメータがあって、かつ、数値形式で、かつ、整数のとき
-// is_int()関数は、「数値文字列」の場合はfalseを返却します。
-// formやgetパラメータで受け取った「数値」は「文字列」になっています。
+// GETパラメータがあって、かつ、数値形式で、かつ、整数のとき。
+// isset()は、変数が存在していて、そして NULLとは異なるときにtrueを返却します。
+// is_numeric()関数は、引数が「数値文字列」の場合にtrueを返却します。
+// is_int()関数は、引数が「数値文字列」の場合はfalseを返却しますので、integerにキャストしています。
+// HTMLのFORMからPOSTまたはGETで送信された値やgetパラメータ（クエリストリング、クエリパラメータ）で受け取った「数値」は「文字列」になっています。
 if (isset($_GET['month']) && is_numeric($_GET['month']) && is_int((int) $_GET['month'])) {
     $month = (int) $_GET['month'];
 }
 
-// 今月の「ついたち」のDateTimeクラスのインスタンスを生成する。
+// 今日の日付のDateTimeクラスのインスタンスを生成します。
+// https://www.php.net/manual/ja/datetime.construct.php
 $dateTime = new DateTime();
-$dateTime = new DateTime($dateTime->format("Y/m") . "/1");
+
+// タイムゾーンを「アジア/東京」にします。DateTimeZoneクラスを使用します。
+// XAMPPのPHPのタイムゾーンは、デフォルトで「ヨーロッパ/ベルリン」になっています。
+// https://www.php.net/manual/ja/datetimezone.construct.php
+$dateTime->setTimezone(new DateTimeZone('Asia/Tokyo'));
+
+// 今日の日付から(今日の日付 - 1)を引き、DateTimeクラスのインスタンスを今月の1日の日付に設定します。
+// 日付のフォーマットの引数の書式は、こちらを参照してください。（date()関数と同じものが使えます。）
+// https://www.php.net/manual/ja/function.date.php
+$d = $dateTime->format('d');
+$dateTime->sub(new DateInterval('P' . ($d - 1) . 'D'));
 
 if ($month > 0) {
-    // $monthが0より大きいときは、現在月の「ついたち」に、その月数を追加する
+    // $monthが0より大きいときは、現在月の「ついたち」に、その月数を追加します。
     $dateTime->add(new DateInterval("P" . $month . "M"));
 } else {
-    // $monthが0より小さいときは、現在月の「ついたち」から、その月数を引く
+    // $monthが0より小さいときは、現在月の「ついたち」から、その月数を引きます。
     $dateTime->sub(new DateInterval("P" . (0 - $month) . "M"));
 }
 
-// 月初の日の曜日＝当月の「ついたち」までに、その週に何日あるか
+// 当月の「ついたち」が何曜日か求めます。当月の「ついたち」までに何日あるか、という日数と等しくなります。
 $beginDayOfWeek = $dateTime->format('w');
 
-// 月末の日＝当月に何日あるかの日数
+// 当月に何日あるかの日数を求めます。
 $monthDays = $dateTime->format('t');
 
-// 当月に何週あるか（日数切り上げ）
+// 当月に何週あるかを求めます。小数点以下を切り上げることで、同月の週数が求められます。
 $weeks = ceil(($monthDays + $beginDayOfWeek) / 7);
 
-// カレンダに記述する日付のカウンタ
+// カレンダに記述する日付のカウンタ。
 $date = 1;
 ?>
 <!DOCTYPE html>
